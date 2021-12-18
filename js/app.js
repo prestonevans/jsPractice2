ef06783a0b5ed3f98f622fbe4ae6fb0d87cb4d34
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-12-18 16:39:50 -0700

    using regex to format a contenteditable

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const h1 = document.querySelector('h1')
+
+// h1.addEventListener('click',()=> {
+//     console.log('clicked')
+// })
+
+h1.addEventListener('keyup',(e) => {
+    if(/[a]/i.test(e.key)) {
+        console.log(e.key)
+        console.log('hey')
+    }
+})
+h1.addEventListener('blur', () => {
+    let string = h1.textContent;
+    string = string.replace(/\s+/g, ' ');
+    string = string.match(/\w+/gi)
+    .map(word => word[0].toUpperCase() + word.slice(1).toLocaleLowerCase())
+    .join(' ')
+    h1.textContent = string;
+})
