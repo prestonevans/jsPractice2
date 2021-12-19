72c1f697bf0756a2b7ca4a76d7e697bde884d49b
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-12-19 15:59:10 -0700

    made a clock with set interval

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
 const h1 = document.querySelector('h1')
 
-// h1.addEventListener('click',()=> {
-//     console.log('clicked')
+// // h1.addEventListener('click',()=> {
+// //     console.log('clicked')
+// // })
+
+// h1.addEventListener('keyup',(e) => {
+//     if(e.key.charCodeAt() >= 65 && e.key.charCodeAt() <= 90) {
+//         console.log(e.key)
+//         console.log('hey')
+//     }
+// })
+// h1.addEventListener('blur', () => {
+//     let string = h1.textContent;
+//     string = string.match(/\w+/gi)
+//     .map(word => word[0].toUpperCase() + word.slice(1).toLocaleLowerCase())
+//     .join(' ')
+//     h1.textContent = string;
 // })
 
-h1.addEventListener('keyup',(e) => {
-    if(/[a]/i.test(e.key)) {
-        console.log(e.key)
-        console.log('hey')
+
+setInterval(() => {
+    let hours = new Date().getHours();
+    if(hours > 12) {
+        hours -= 12;
     }
-})
-h1.addEventListener('blur', () => {
-    let string = h1.textContent;
-    string = string.replace(/\s+/g, ' ');
-    string = string.match(/\w+/gi)
-    .map(word => word[0].toUpperCase() + word.slice(1).toLocaleLowerCase())
-    .join(' ')
-    h1.textContent = string;
-})
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+    h1.innerText = `${hours}:${minutes}:${seconds}`;
+}, 1000);
